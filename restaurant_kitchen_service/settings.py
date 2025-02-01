122bdfe2f15bb885aaf27c485d7525837cdf3cc6
Ruslan <ruslanyemets@gmail.com>
2025-02-01 22:50:24 +0200

    Add kitchen to INSTALLED_APPS and AUTH_USER_MODEL to settings

--- restaurant_kitchen_service/settings.py
+++ restaurant_kitchen_service/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "kitchen",
 ]
 
 MIDDLEWARE = [
@@ -97,6 +98,8 @@ AUTH_PASSWORD_VALIDATORS = [
         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
+
+AUTH_USER_MODEL = "kitchen.Cook"
 
 
 # Internationalization
