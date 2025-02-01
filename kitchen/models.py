9ec63bd72f8dd2927a56b7cb523489ff445d1c75
Ruslan <ruslanyemets@gmail.com>
2025-02-01 23:08:05 +0200

    Add models DishType, Cook, Dish, make initial migration, migrate, add db.sqlite3 to gitignore

--- kitchen/models.py
+++ kitchen/models.py
@@ -1,3 +1,39 @@
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class DishType(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+class Cook(AbstractUser):
+    years_of_experience = models.IntegerField()
+
+    class Meta:
+        verbose_name = "cook"
+        verbose_name_plural = "cooks"
+
+    def __str__(self):
+        return f"{self.username} ({self.first_name} {self.last_name})"
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    dish_type = models.ForeignKey(
+        DishType,
+        on_delete=models.CASCADE,
+        related_name="dishes"
+    )
+    cooks = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="dishes")
+
+    def __str__(self):
+        return self.name
