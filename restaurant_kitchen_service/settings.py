4285075d45422621e76a1055396d44649ec60810
Ruslan <ruslanyemets@gmail.com>
2025-02-02 17:51:48 +0200

    Add django debug toolbar to installed apps, add debug toolbar middleware, internal ip, update project urls

--- restaurant_kitchen_service/settings.py
+++ restaurant_kitchen_service/settings.py
@@ -27,6 +27,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
+
 
 # Application definition
 
@@ -38,10 +42,12 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "django.contrib.admin",
     "kitchen",
+    "debug_toolbar",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
--- restaurant_kitchen_service/urls.py
+++ restaurant_kitchen_service/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from debug_toolbar.toolbar import debug_toolbar_urls
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
@@ -23,4 +24,7 @@ from django.urls import path, include
 urlpatterns = [
        path("admin/", admin.site.urls),
        path("", include("kitchen.urls", namespace="kitchen"))
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(
+       settings.STATIC_URL,
+       document_root=settings.STATIC_ROOT
+) + debug_toolbar_urls()
