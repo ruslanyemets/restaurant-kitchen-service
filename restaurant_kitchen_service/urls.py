57dd9c85e6d4e7304bf2e8c1e7ba6b96c917bc59
Ruslan <ruslanyemets@gmail.com>
2025-02-02 17:25:41 +0200

    Add static folder with css folder and styles.css, update settings, project urls, load static in base template

--- restaurant_kitchen_service/urls.py
+++ restaurant_kitchen_service/urls.py
@@ -14,7 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -22,4 +23,4 @@ from django.urls import path, include
 urlpatterns = [
        path("admin/", admin.site.urls),
        path("", include("kitchen.urls", namespace="kitchen"))
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
